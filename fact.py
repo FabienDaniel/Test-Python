1b639cde1f02f8045e0f3669950365c03d89bd05
Fabien_Daniel <fabien128@gmail.com>
2016-10-17 15:40:50 +0200

    creation de la fonction fact.py appelée par main.py

---
+++ fact.py
@@ -0,0 +1,16 @@
+#!/usr/local/bin/python3
+# -*-coding:Utf-8 -*
+
+def factorielle(vinit) :
+  fac    = 1
+  valeur = vinit
+  while valeur >= 1 :
+    fac = fac*valeur
+    valeur = valeur-1
+    print("ICI : ",fac)
+
+  return fac
+
+
+
+
--- main.py
+++ main.py
@@ -1,18 +1,16 @@
 #!/usr/local/bin/python3
 # -*-coding:Utf-8 -*
 
+from fact import factorielle
 
 print("Bonjour ; voilà une fonction calculant des factorielles (normalement ...)")
 
-fac    = 1
-vinit  = 4  
-valeur = vinit
-while valeur >= 1 :
-  fac = fac*valeur
-  valeur = valeur-1
-  print("ICI : ",fac)
+vinit = 4
+fac = factorielle(vinit)
 
 print("la factorielle de {} vaut {} ".format(vinit,fac))
+
+
 
 
 
